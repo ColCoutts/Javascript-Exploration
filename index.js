ee1e4e34b20d69900d68f38e6ca388648c14f6b9
Colin Coutts <couttstcolin@gmail.com>
2019-01-24 16:09:08 -0800

    playin with javascript variables and functions

---
+++ index.js
@@ -0,0 +1,33 @@
+var x = 5;
+    if (x < 5){
+        console.log('sorry sir');
+    }
+
+var d = new Date();
+var time = d.getHours();
+    if (time < 20){
+        console.log("hello nurse!");
+    }
+console.log(time);
+
+
+function even (){
+    var y = 7
+
+    if (y % 2 === 0){
+        console.log("even");
+    } else {
+        console.log ("odd");
+    }
+}
+
+even();
+
+for(var i =0; i < y; i++){
+    if(i % 2 ===0){
+        console.log(i);
+    }
+    else{
+        console.log('odd');
+    }
+}
