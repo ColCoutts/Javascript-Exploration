7d23bbe02cd6cac9b460d6473b4196db275e223e
Colin Coutts <couttstcolin@gmail.com>
2019-01-27 15:02:25 -0800

    more if and for statements

--- index.js
+++ index.js
@@ -38,8 +38,40 @@ function carTypes(name){
     if (name === 'Honda'){
         console.log(name);
     } else {
-        console.log("Sorry, we don't sell" + name + ".");
+        console.log("Sorry, we don't sell " + name + ".");
     }
 }
 
 carTypes('toyota');
+
+
+for(var i = 0; i < 7; i++){
+    console.log(i);
+}
+
+var str = " ";
+
+for(var i = 0; i < 8; i++){
+    str = str + i;
+}
+
+console.log(str);
+
+var num = 5
+
+for(var i = 0; i <9; i++){
+    num = num + i;
+}
+console.log(num);
+
+var phrase = ['Hello Nurse', 'Yes Maam', 'No Sir', 'Sorry', 'Please dont be that way', 'Oh God Help Me'];
+
+for(var i = 0; i < phrase.length; i++){
+    if(i < 3){
+        console.log(phrase[i]);
+    } else if (i < 5){
+        console.log(phrase[i]);
+    } else if (i > 5){
+        console.log(phrase[i]);
+    }
+}
